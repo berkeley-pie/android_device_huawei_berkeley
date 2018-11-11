d9975339c6d2f373976ad8ead18294267b452350
David Sn <divad.nnamtdeis@gmail.com>
2018-11-11 18:45:58 +0100

    berkeley: Nuke lineage stuff and unused overlays

--- aosp_berkeley.mk
+++ aosp_berkeley.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,18 +19,11 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-TARGET_AOSP_BASED := true
-
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_berkeley
 PRODUCT_DEVICE := berkeley
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Honor View 10
-
-PRODUCT_PACKAGES += \
-    Launcher3 \
-    Eleven \
-    Jelly
