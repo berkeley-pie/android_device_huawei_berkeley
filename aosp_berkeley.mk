ab304f8bc77fae256a7912d0499b899f73e9c48d
David Sn <divad.nnamtdeis@gmail.com>
2018-11-12 22:38:57 +0100

    berkeley: Copy APN configs from prebuilt

--- aosp_berkeley.mk
+++ aosp_berkeley.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+    vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 $(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
 
