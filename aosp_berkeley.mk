7506bba7dcfdea8916373644eb490dfff9d03c7c
David Sn <divad.nnamtdeis@gmail.com>
2018-11-17 04:22:51 +0100

    berkeley: Add manufacturer info

--- aosp_berkeley.mk
+++ aosp_berkeley.mk
@@ -14,9 +14,11 @@
 # limitations under the License.
 #
 
+# Get the long list of APNs
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# Inherit AOSP device configuration for berkeley
 $(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
 
 # Inherit some common AOSP stuff.
@@ -30,3 +32,6 @@ PRODUCT_NAME := aosp_berkeley
 PRODUCT_DEVICE := berkeley
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Honor View 10
+PRODUCT_MANUFACTURER := Huawei
+
+PRODUCT_GMS_CLIENTID_BASE := android-huawei
